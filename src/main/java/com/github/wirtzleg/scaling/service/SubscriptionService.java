004b2cc1b98fb474150b29ce895e38cba324685f
wirtzleg <akozhenkov@gridgain.com>
2021-08-31 18:59:29 +0300

    Usa shedlock manually.

--- src/main/java/com/github/wirtzleg/scaling/service/SubscriptionService.java
+++ src/main/java/com/github/wirtzleg/scaling/service/SubscriptionService.java
@@ -2,10 +2,13 @@ package com.github.wirtzleg.scaling.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
-import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import net.javacrumbs.shedlock.core.LockConfiguration;
+import net.javacrumbs.shedlock.core.LockProvider;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
 
 import static com.github.wirtzleg.scaling.dto.Subscription.Status.ACTIVE;
 import static com.github.wirtzleg.scaling.dto.Subscription.Status.PAST_DUE;
@@ -16,20 +19,33 @@ import static com.github.wirtzleg.scaling.dto.Subscription.Status.PAST_DUE;
 public class SubscriptionService {
     private final UserService userService;
     private final StripeGateway stripeGateway;
+    private final LockProvider lockProvider;
 
     @Scheduled(cron = "*/30 * * * * *")
-    @SchedulerLock(name = "checkSubscriptions", lockAtLeastFor = "10s", lockAtMostFor = "20s")
     public void checkSubscriptions() {
-        log.info("Checking subscriptions...");
+        LockConfiguration lockCfg = new LockConfiguration(
+                Instant.now(),
+                "checkSubscriptions",
+                Duration.ofSeconds(20),
+                Duration.ofSeconds(10)
+        );
 
-        stripeGateway.getSubscriptions().forEach(sub -> {
-            userService.getById(sub.getUserId()).ifPresent(user -> {
-                if (sub.getStatus() == ACTIVE && user.isExpired())
-                    userService.updateUser(user.setExpired(false));
+        lockProvider.lock(lockCfg).ifPresent(lock -> {
+            log.info("Checking subscriptions...");
 
-                if (sub.getStatus() == PAST_DUE && !user.isExpired())
-                    userService.updateUser(user.setExpired(true));
-            });
+            try {
+                stripeGateway.getSubscriptions().forEach(sub -> {
+                    userService.getById(sub.getUserId()).ifPresent(user -> {
+                        if (sub.getStatus() == ACTIVE && user.isExpired())
+                            userService.updateUser(user.setExpired(false));
+
+                        if (sub.getStatus() == PAST_DUE && !user.isExpired())
+                            userService.updateUser(user.setExpired(true));
+                    });
+                });
+            } finally {
+                lock.unlock();
+            }
         });
     }
 }
