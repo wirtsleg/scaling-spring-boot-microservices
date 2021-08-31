29bf0d8600215c3ab1cc91a960021ffe4dd65830
wirtzleg <akozhenkov@gridgain.com>
2021-08-31 18:33:41 +0300

    Add select-for-update.

--- src/main/java/com/github/wirtzleg/scaling/repository/EmailRepository.java
+++ src/main/java/com/github/wirtzleg/scaling/repository/EmailRepository.java
@@ -1,13 +1,23 @@
 package com.github.wirtzleg.scaling.repository;
 
-import com.github.wirtzleg.scaling.dto.ChatMessage;
 import com.github.wirtzleg.scaling.dto.Email;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.Instant;
 import java.util.List;
 
 public interface EmailRepository extends CrudRepository<Email, Long> {
 
-    List<Email> findAllByStatus(String status, Pageable pageable);
+    @Query("""
+        UPDATE emails
+        SET lock_until = :lockUntil
+        WHERE id in (
+            SELECT e.id FROM emails e
+            WHERE e.status = 'NEW' and (e.lock_until is null or e.lock_until < now())
+            LIMIT :limit
+            FOR UPDATE SKIP LOCKED)
+        RETURNING *
+    """)
+    List<Email> findAllByStatus(String status, Instant lockUntil, int limit);
 }
