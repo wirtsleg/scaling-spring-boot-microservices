1b344821b3e815aa6d91a0eb88f367587da4dac9
wirtzleg <akozhenkov@gridgain.com>
2021-10-10 13:03:41 +0300

    Fix imports.

--- src/main/java/com/github/wirtzleg/scaling/repository/EmailRepository.java
+++ src/main/java/com/github/wirtzleg/scaling/repository/EmailRepository.java
@@ -1,11 +1,9 @@
 package com.github.wirtzleg.scaling.repository;
 
-import com.github.wirtzleg.scaling.dto.ChatMessage;
+import java.util.List;
 import com.github.wirtzleg.scaling.dto.Email;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 public interface EmailRepository extends CrudRepository<Email, Long> {
 
